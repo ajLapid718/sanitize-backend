6808eefd4ca057476b68c24796ddae6bed23ad1b
ajLapid718 <ajLapid718@gmail.com>
2020-01-18 19:16:56 -0500

    make a route to post new items

--- routes/items.js
+++ routes/items.js
@@ -5,8 +5,14 @@ const { Item } = require('../database/models');
 router.get('/', function(req, res, next) {
   Item.findAll()
     .then(items => res.json(items))
-    .catch(err => console.log(err))
+    .catch(err => next(err));
 });
+
+router.post('/', function(req, res, next) {
+  Item.create(req.body)
+    .then(createdItem => res.json(createdItem))
+    .catch(err => next(err));
+})
 
 // Export our router, so that it can be imported to construct our apiRouter;
 module.exports = router;
