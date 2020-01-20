b0e4006445d6340bea8641f286fa8f1ccee0a28f
Allan James Lapid <ajLapid718@gmail.com>
2020-01-19 23:19:28 -0500

    ensure the database is seeded whether or not the db already exists

--- utilities/seedDatabase.js
+++ utilities/seedDatabase.js
@@ -1,20 +1,29 @@
 const { Item } = require('../database/models');
 
 const seedDatabase = async () => {
-  await Promise.all([
-    Item.create({
-      name: "Macbook",
-      price: 1050
-    }),
-    Item.create({
-      name: "Apple",
-      price: 20
-    }),
-    Item.create({
-      name: "Banana",
-      price: 10
-    })
-  ]);
+  console.log("currently seeding the database!!!");
+
+  try {
+    await Promise.all([
+      Item.create({
+        name: "Macbook",
+        price: 1050
+      }),
+      Item.create({
+        name: "Apple",
+        price: 20
+      }),
+      Item.create({
+        name: "Banana",
+        price: 10
+      })
+    ]);
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+  console.log("successfully seeded the database!!!");
 }
 
 module.exports = seedDatabase;
